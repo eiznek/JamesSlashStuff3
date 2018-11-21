7877aecb72095a8001d416b20310352f9e1ab8d1
Lee Jia Keat <s10177804@connect.np.edu.sg>
2018-11-21 17:33:30 +0800

    Transform Class

--- EnginePart1/EnginePart1/Transform.h
+++ EnginePart1/EnginePart1/Transform.h
@@ -6,10 +6,53 @@ class Transform {
 	private:
 		VECTOR2 *position;
 
-	public:
-		void Translate(VECTOR2 translation);
-		VECTOR2 GetPosition();
+	public:		
 		Transform();
-		virtual ~Transform();
+		~Transform() { Reset(); }
+
+		void Reset();
+
+		void TranslateAbs(float x, float y, float z);
+
+		void TranslateRel(float x, float y, float z);
+
+		void RotateAbs(float x, float y, float z);
+
+		void RotateRel(float x, float y, float z);
+
+		void ScaleAbs(float x, float y, float z);
+
+		void ScaleRel(float x, float y, float z);
+
+		D3DXMATRIX* GetTransform();
+
+		float GetXPosition() { return m_translate._41; }
+
+		float GetYPosition() { return m_translate._42; }
+
+		float GetZPosition() { return m_translate._43; }
+
+		float GetXRotation() { return m_rotationX; }
+
+		float GetYRotation() { return m_rotationY; }
+
+		float GetZRotation() { return m_rotationZ; }
+
+		float GetXScale() { return m_scale._11; }
+
+		float GetYScale() { return m_scale._22; }
+
+		float GetZScale() { return m_scale._33; }
+
+	protected:
+		D3DXMATRIX m_translate;
+
+		D3DXMATRIX m_rotate;
+
+		D3DXMATRIX m_scale;
+
+		D3DXMATRIX m_transform;
+
+		float m_rotationX, m_rotationY, m_rotationZ;
 
 };
